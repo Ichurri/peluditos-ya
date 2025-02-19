2ee912c4938622d998906569598101ab7af03562
Ichurri <santiagoiturrivargas04@gmail.com>
2025-02-18 20:07:39 -0400

    first commit, creating springboot-mvn backend, working with the following dependencies: - Spring WEb - Spring Data JPA - Spring Boot DevTools - PostgreSQL Driver

---
+++ peluditos-ya-server/src/main/java/com/hilr/peluditos_ya_server/PeluditosYaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hilr.peluditos_ya_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluditosYaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluditosYaServerApplication.class, args);
+	}
+
+}
---
+++ peluditos-ya-server/src/test/java/com/hilr/peluditos_ya_server/PeluditosYaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilr.peluditos_ya_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeluditosYaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
