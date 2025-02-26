32663a24c80b3202531ad8a814ebb9dd2ff3e061
Ichurri <santiagoiturrivargas04@gmail.com>
2025-02-26 16:49:26 -0400

    Create basic backend : Springboot, for one class: Pet

---
+++ peluditos-ya-server/src/main/java/com/hilr/peluditos_ya_server/repository/MascotaRepository.java
@@ -0,0 +1,15 @@
+package com.hilr.peluditos_ya_server.repository;
+
+import com.hilr.peluditos_ya_server.model.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Long> {
+    // Custom queries
+    List<Mascota> findByEspecie(String especie);
+    List<Mascota> findByEstado(String estado);
+    List<Mascota> findByEspecieAndEstado(String especie, String estado);
+}
