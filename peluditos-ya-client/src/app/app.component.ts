61251bc84ae111e9b1d204a73214b7d0fd2f7130
BetoHerbas <betitoherbas@gmail.com>
2025-03-04 01:30:53 -0400

    refactor(app): move inline template to a separate HTML file for better organization

--- peluditos-ya-client/src/app/app.component.ts
+++ peluditos-ya-client/src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-  `,
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
