2efbc0b126c081b9920abe88cc35918d8a54b91c
BetoHerbas <betitoherbas@gmail.com>
2025-03-04 22:29:47 -0400

    Improve homepage layout: Add dynamic background carousel, adjust text positioning, and refine footer behavior

--- peluditos-ya-client/src/app/app.routes.ts
+++ peluditos-ya-client/src/app/app.routes.ts
@@ -5,7 +5,6 @@ export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        pathMatch: 'full',
         title: 'Home'
     },
     { path: '**', redirectTo: '' }
--- peluditos-ya-client/src/app/pages/home/home.component.ts
+++ peluditos-ya-client/src/app/pages/home/home.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,  // 🔹 Indica que es un componente independiente
+  imports: [CommonModule, FooterComponent],  // 🔹 Importa CommonModule para habilitar directivas como ngStyle
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  imagenes: string[] = [
+    'https://patitasalrescateorg.netlify.app/img/PrimerFondo.jpg',
+    'https://cdn.pixabay.com/photo/2022/06/29/14/03/puppy-7291850_1280.jpg',
+    '	https://cdn.pixabay.com/photo/2018/02/08/16/09/dog-3139757_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/10/06/11/50/dog-5632005_960_720.jpg'
+    
+  ];
+  
+  indiceImagenActual = 0;
+  
+  ngOnInit() {
+    setInterval(() => {
+      this.indiceImagenActual = (this.indiceImagenActual + 1) % this.imagenes.length;
+    }, 5000); 
+  }
 }
