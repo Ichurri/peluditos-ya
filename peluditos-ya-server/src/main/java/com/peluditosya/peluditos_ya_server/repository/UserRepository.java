2c44521b627ad7a1d202f702744c788d72086eee
Ichurri <santiagoiturrivargas04@gmail.com>
2025-03-20 18:00:12 -0400

    T2.2 add Shelter methods to UserRepository

--- peluditos-ya-server/src/main/java/com/peluditosya/peluditos_ya_server/repository/UserRepository.java
+++ peluditos-ya-server/src/main/java/com/peluditosya/peluditos_ya_server/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.peluditosya.peluditos_ya_server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.peluditosya.peluditos_ya_server.model.AppUser;
+import com.peluditosya.peluditos_ya_server.model.Shelter;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     boolean existsByEmail(String email);
     AppUser findByEmailAndPassword(String email, String password);
+
+    @Query("SELECT s FROM Shelter s WHERE s.role = 'REFUGIO'")
+    List<Shelter> findAllShelters();
 }
