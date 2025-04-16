f2b8b86b4208b68230049185310e492ad99b675d
Ichurri <santiagoiturrivargas04@gmail.com>
2025-04-16 16:50:24 -0400

    Remove unused imports

--- peluditos-ya-server/src/main/java/com/peluditosya/peluditos_ya_server/dto/AdopterSignUpRequest.java
+++ peluditos-ya-server/src/main/java/com/peluditosya/peluditos_ya_server/dto/AdopterSignUpRequest.java
@@ -1,16 +1,12 @@
 package com.peluditosya.peluditos_ya_server.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class AdopterSignUpRequest extends UserSignUpRequest {
-    @NotBlank(message = "El teléfono es obligatorio")
-    @Pattern(regexp = "^[0-9]+$", message = "El teléfono solo debe contener números")
+
     private String phone;
+
+    public String getPhone(){
+        return this.phone;
+    }
 
     public void setPhone(String phone) {
         this.phone = phone;
