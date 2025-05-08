29f5a32d3af67b7156a379e15bc4f55c1a6419f3
Ichurri <santiagoiturrivargas04@gmail.com>
2025-05-08 13:24:01 -0400

    Create Pet Profile Component

---
+++ peluditos-ya-client/src/app/pages/pet-profile/pet-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-profile',
+  imports: [],
+  templateUrl: './pet-profile.component.html',
+  styleUrl: './pet-profile.component.css'
+})
+export class PetProfileComponent {
+
+}
