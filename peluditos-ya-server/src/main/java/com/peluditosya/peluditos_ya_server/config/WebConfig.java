dac05a14fd6b7afe6772af4912c7073357731b35
BetoHerbas <betitoherbas@gmail.com>
2025-05-19 11:00:27 -0400

    Updating allowedOrigins custom domain

---
+++ peluditos-ya-server/src/main/java/com/peluditosya/peluditos_ya_server/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.peluditosya.peluditos_ya_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://lemon-pebble-06ee7b410.6.azurestaticapps.net")
+                        .allowedOrigins("https://www.peluditosya.publicvm.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
